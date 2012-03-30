632ce3845560a74636be680da344227be604ac4e
Matt Campbell <matt@soupmatt.com>
2012-03-30 15:06:58 -0500

    Chapter 9

--- spec/codebreaker/marker_spec.rb
+++ spec/codebreaker/marker_spec.rb
@@ -28,6 +28,7 @@ module Codebreaker
         end
       end
     end
+
     describe "#number_match_count" do
       context "with no matches" do
         it "returns 0" do
@@ -51,6 +52,12 @@ module Codebreaker
         it "returns 1" do
           marker = Marker.new('1234','1525')
           marker.number_match_count.should == 1
+        end
+      end
+      context "with 1 exact match duplicated in guess" do
+        it "returns 0" do
+          marker = Marker.new('1234','1155')
+          marker.number_match_count.should == 0
         end
       end
     end
